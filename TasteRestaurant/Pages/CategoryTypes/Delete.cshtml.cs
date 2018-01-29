9baea6f07e0c9a5ed6a2d647b82bddb0c11018cc
Ben Spark <bhrugen1990@gmail.com>
2018-01-29 02:03:29 -0600

    Section 5 - Category Type Delete Page Model

---
+++ TasteRestaurant/Pages/CategoryTypes/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TasteRestaurant.Data;
+
+namespace TasteRestaurant.Pages.CategoryTypes
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public CategoryType CategoryType { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            CategoryType = await _db.CategoryType.SingleOrDefaultAsync(c => c.Id == id);
+
+            if (CategoryType == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            CategoryType = await _db.CategoryType.FindAsync(id);
+
+            if (CategoryType != null)
+            {
+                _db.CategoryType.Remove(CategoryType);
+                await _db.SaveChangesAsync();
+            }
+            return RedirectToPage("./Index");
+        }
+    }
+}
