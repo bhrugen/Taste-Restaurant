17e5fc377c5f305bf9aa0f1530107dc1de6d31e2
Ben Spark <bhrugen1990@gmail.com>
2018-01-29 14:27:58 -0600

    Section 7 - Menu Item - Index Page Model

---
+++ TasteRestaurant/Pages/MenuItems/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TasteRestaurant.Data;
+
+namespace TasteRestaurant.Pages.MenuItems
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IList<MenuItem> MenuItem { get; set; }
+
+        public async Task OnGet()
+        {
+            MenuItem = await _db.MenuItem
+                .Include(m=>m.CategoryType)
+                .Include(m=>m.FoodType)
+                .ToListAsync();
+        }
+    }
+}
