0b4e5c8b370aba07d968de938c168918474092bf
Ben Spark <bhrugen1990@gmail.com>
2018-01-29 01:36:57 -0600

    Section 5 - Category Type Edit Page Model

--- TasteRestaurant/Pages/CategoryTypes/Edit.cshtml.cs
+++ TasteRestaurant/Pages/CategoryTypes/Edit.cshtml.cs
@@ -4,14 +4,52 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TasteRestaurant.Data;
 
 namespace TasteRestaurant.Pages.CategoryTypes
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext _db;
 
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public CategoryType CategoryType { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            CategoryType = await _db.CategoryType.SingleOrDefaultAsync(c => c.Id == id);
+
+            if (CategoryType == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Attach(CategoryType).State = EntityState.Modified;
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
