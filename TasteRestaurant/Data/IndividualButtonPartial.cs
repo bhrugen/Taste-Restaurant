97b2e4d2b5059acf639dca171df264b2b7b27569
Ben Spark <bhrugen1990@gmail.com>
2018-01-29 00:40:03 -0600

    Section 5 - Individual Button Partial

---
+++ TasteRestaurant/Data/IndividualButtonPartial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TasteRestaurant.Data
+{
+    public class IndividualButtonPartial
+    {
+        public string Page { get; set; }
+        public string Glyph { get; set; }
+        public string ButtonType { get; set; }
+        
+        public int? Id { get; set; }
+
+        public string ActionParameters
+        {
+            get
+            {
+                if(Id!=0 && Id !=null)
+                {
+                    return Id.ToString();
+                }
+                return null;
+            }
+        }
+    }
+}
