6ed91aa67e5b95c13c770eb7598ea4dc1c1d745d
Ben Spark <bhrugen1990@gmail.com>
2018-01-29 14:17:47 -0600

    Section 7 - MenuItem View Model

---
+++ TasteRestaurant/ViewModel/MenuItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TasteRestaurant.Data;
+
+namespace TasteRestaurant.ViewModel
+{
+    public class MenuItemViewModel
+    {
+        public MenuItem MenuItem { get; set; }
+        public IEnumerable<CategoryType> CategoryType { get; set; }
+        public IEnumerable<FoodType> FoodType { get; set; }
+    }
+}
