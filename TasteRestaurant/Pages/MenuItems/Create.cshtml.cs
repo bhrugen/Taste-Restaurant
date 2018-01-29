0d95ecda1f3a0f8b10db21353ab67763be57a169
Ben Spark <bhrugen1990@gmail.com>
2018-01-29 15:59:08 -0600

    Section 7 - Menu Item - Create Page Model Post

--- TasteRestaurant/Pages/MenuItems/Create.cshtml.cs
+++ TasteRestaurant/Pages/MenuItems/Create.cshtml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TasteRestaurant.Data;
+using TasteRestaurant.Utility;
 using TasteRestaurant.ViewModel;
 
 namespace TasteRestaurant.Pages.MenuItems
@@ -34,5 +36,45 @@ namespace TasteRestaurant.Pages.MenuItems
 
         [BindProperty]
         public MenuItemViewModel MenuItemVM { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.MenuItem.Add(MenuItemVM.MenuItem);
+            await _db.SaveChangesAsync();
+
+            //Image Being Saved
+
+            string webRootPath = _hostingEnvironment.WebRootPath;
+
+            var files = HttpContext.Request.Form.Files;
+
+            var menuItemFromDb = _db.MenuItem.Find(MenuItemVM.MenuItem.Id);
+
+            if (files[0] != null && files[0].Length > 0)
+            {
+                var uploads = Path.Combine(webRootPath, "images");
+                var extension = files[0].FileName.Substring(files[0].FileName.LastIndexOf("."), files[0].FileName.Length - files[0].FileName.LastIndexOf("."));
+
+                using (var fileStream = new FileStream(Path.Combine(uploads, MenuItemVM.MenuItem.Id + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(fileStream);
+                }
+                menuItemFromDb.Image = @"\images\" + MenuItemVM.MenuItem.Id + extension;
+            }
+            else
+            {
+                var uploads = Path.Combine(webRootPath, @"images\" + SD.DefaultFoodImage);
+                System.IO.File.Copy(uploads, webRootPath + @"\images\" + MenuItemVM.MenuItem.Id + ".png");
+                menuItemFromDb.Image = @"\images\" + MenuItemVM.MenuItem.Id + ".png";
+            }
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
     }
 }
---
+++ TasteRestaurant/Utility/SD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TasteRestaurant.Utility
+{
+    public class SD
+    {
+
+        public const string DefaultFoodImage = "default_food.png";
+
+    }
+}
