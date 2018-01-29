134b5888d2fc6cf67dddcd326ba6bc4f82dc4e34
Ben Spark <bhrugen1990@gmail.com>
2018-01-29 00:12:37 -0600

    Section 5 - Category Type Index Page Model

---
+++ TasteRestaurant/Pages/CategoryTypes/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TasteRestaurant.Data;
+
+namespace TasteRestaurant.Pages.CategoryTypes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IList<CategoryType> CategoryType { get; set; }
+
+        public async Task OnGet()
+        {
+            CategoryType = await _db.CategoryType.OrderBy(c => c.DisplayOrder).ToListAsync();
+        }
+    }
+}
