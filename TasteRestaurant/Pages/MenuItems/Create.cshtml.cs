6d5881cb78ab43d998e2fc89714f499f8d068605
Ben Spark <bhrugen1990@gmail.com>
2018-01-29 14:52:14 -0600

    Section 7 - Menu Item : Create Page Model Get

---
+++ TasteRestaurant/Pages/MenuItems/Create.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TasteRestaurant.Data;
+using TasteRestaurant.ViewModel;
+
+namespace TasteRestaurant.Pages.MenuItems
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public CreateModel( ApplicationDbContext db, IHostingEnvironment hostingEnvironment)
+        {
+            _db = db;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult OnGet()
+        {
+            MenuItemVM = new MenuItemViewModel
+            {
+                MenuItem = new MenuItem(),
+                FoodType = _db.FoodType.ToList(),
+                CategoryType = _db.CategoryType.ToList()
+            };
+            return Page();
+        }
+
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
+    }
+}
