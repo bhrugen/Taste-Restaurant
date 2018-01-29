06acfc2e70499a608bd465c208d9cb6d741a0c90
Ben Spark <bhrugen1990@gmail.com>
2018-01-29 01:50:04 -0600

    Section 5 - Category Type Details Page Model

---
+++ TasteRestaurant/Pages/CategoryTypes/Details.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TasteRestaurant.Data;
+
+namespace TasteRestaurant.Pages.CategoryTypes
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DetailsModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public CategoryType CategoryType { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            CategoryType = await _db.CategoryType.SingleOrDefaultAsync(c => c.Id == id);
+
+            if (CategoryType == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+    }
+}
