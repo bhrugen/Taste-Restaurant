c04835e15c1398e585bce45c0b2421040433504c
Ben Spark <bhrugen1990@gmail.com>
2018-01-29 15:12:08 -0600

    Section 7 - Extension Methods

---
+++ TasteRestaurant/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TasteRestaurant.Extensions
+{
+    public static class ReflectionExtensions
+    {
+        public static string GetPropertyValue<T>(this T item, string propertyName)
+        {
+            return item.GetType().GetProperty(propertyName).GetValue(item, null).ToString();
+        }
+    }
+}
