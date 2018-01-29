2a71ad15a71be8d9652b5627781bfa722116e2b5
Ben Spark <bhrugen1990@gmail.com>
2018-01-29 17:13:34 -0600

    Section 7 - Menu Item : Details Page Model

---
+++ TasteRestaurant/Pages/MenuItems/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TasteRestaurant.Data;
+
+namespace TasteRestaurant.Pages.MenuItems
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public DetailsModel(ApplicationDbContext db, IHostingEnvironment hostingEnvironment)
+        {
+            _db = db;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public MenuItem MenuItem { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            MenuItem = await _db.MenuItem.Include(m => m.CategoryType).Include(m => m.FoodType).SingleOrDefaultAsync(m => m.Id == id);
+
+            if (MenuItem == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+    }
+}
