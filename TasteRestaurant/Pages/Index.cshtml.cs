bb109282cf62a2ffb05246cbcd9e481190d2d89d
Ben Spark <bhrugen1990@gmail.com>
2018-01-31 16:23:24 -0600

    Section 9 - Index Page Model Get Method and Razor Page

--- TasteRestaurant/Pages/Index.cshtml.cs
+++ TasteRestaurant/Pages/Index.cshtml.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TasteRestaurant.Data;
+using TasteRestaurant.ViewModel;
 
 namespace TasteRestaurant.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext _db;
 
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public IndexViewModel IndexVM { get; set; }
+
+        public async Task OnGet()
+        {
+            IndexVM = new IndexViewModel() {
+                MenuItems = await _db.MenuItem.Include(m => m.CategoryType).Include(m => m.FoodType).ToListAsync(),
+                CategoryTypes = _db.CategoryType.OrderBy(c => c.DisplayOrder)
+            };
         }
     }
 }
