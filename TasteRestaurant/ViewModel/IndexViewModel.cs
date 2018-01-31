fa55b0c883828e642887050bfc546157a073b1ed
Ben Spark <bhrugen1990@gmail.com>
2018-01-31 16:15:12 -0600

    Section 9 - Create IndexViewModel

---
+++ TasteRestaurant/ViewModel/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TasteRestaurant.Data;
+
+namespace TasteRestaurant.ViewModel
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<MenuItem> MenuItems { get; set; }
+        public IEnumerable<CategoryType> CategoryTypes { get; set; }
+    }
+}
