6cda3c3dfbf35e6e42bd4f766b7e10706d45b2b3
Ben Spark <bhrugen1990@gmail.com>
2018-01-31 14:41:44 -0600

    Section Email Sender Class

--- TasteRestaurant/Services/EmailSender.cs
+++ TasteRestaurant/Services/EmailSender.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace TasteRestaurant.Services
@@ -11,6 +13,18 @@ namespace TasteRestaurant.Services
     {
         public Task SendEmailAsync(string email, string subject, string message)
         {
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.UseDefaultCredentials=false;
+            client.Credentials = new NetworkCredential("bhrugentemp@gmail.com", "Changeme123");
+            client.EnableSsl = true;
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("bhrugentemp@gmail.com");
+            mailMessage.To.Add(email);
+            mailMessage.Body = message;
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Subject = subject;
+            client.Send(mailMessage);
             return Task.CompletedTask;
         }
     }
