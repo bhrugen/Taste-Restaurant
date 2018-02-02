07e0684a799814b8014c2a805c955276db95417d
Ben Spark <bhrugen1990@gmail.com>
2018-02-02 17:35:44 -0600

    Section 9 - Details Razor Page Model Get Handler

---
+++ TasteRestaurant/Pages/Details.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TasteRestaurant.Data;
+
+namespace TasteRestaurant.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DetailsModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public ShoppingCart CartObj { get; set; }
+
+        public void OnGet(int id)
+        {
+            var MenuItemFromDb=_db.MenuItem.Include(m=>m.CategoryType).Include(m=>m.FoodType).FirstOrDefault();
+
+
+            CartObj = new ShoppingCart()
+            {
+                MenuItemId=MenuItemFromDb.Id,
+                MenuItem=MenuItemFromDb
+            };
+
+        }
+    }
+}
