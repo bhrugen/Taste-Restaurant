129feeae27452a8e76f346287314c7d238eafbfd
Ben Spark <bhrugen1990@gmail.com>
2018-02-02 18:10:18 -0600

    Secton 9 - Details Razor Page Model Post Handler

--- TasteRestaurant/Pages/Details.cshtml.cs
+++ TasteRestaurant/Pages/Details.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -31,7 +32,46 @@ namespace TasteRestaurant.Pages
                 MenuItemId=MenuItemFromDb.Id,
                 MenuItem=MenuItemFromDb
             };
+        }
 
+
+        public IActionResult OnPost()
+        {
+
+            if (ModelState.IsValid)
+            {
+                var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+                var claim = claimsIdentity.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
+                CartObj.ApplicationUserId = claim.Value;
+
+                ShoppingCart cartFromDb = _db.ShoppingCart.Where(c => c.ApplicationUserId == CartObj.ApplicationUserId
+                                            && c.MenuItemId == CartObj.MenuItemId).FirstOrDefault();
+
+                if (cartFromDb == null)
+                {
+                    _db.ShoppingCart.Add(CartObj);
+                }
+                else
+                {
+                    cartFromDb.Count = cartFromDb.Count + CartObj.Count;
+                }
+                _db.SaveChanges();
+
+                //Add Session and increment count
+
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                var MenuItemFromDb = _db.MenuItem.Include(m => m.CategoryType).Include(m => m.FoodType).FirstOrDefault();
+
+
+                CartObj = new ShoppingCart()
+                {
+                    MenuItemId = MenuItemFromDb.Id,
+                    MenuItem = MenuItemFromDb
+                };
+            }
         }
     }
 }
