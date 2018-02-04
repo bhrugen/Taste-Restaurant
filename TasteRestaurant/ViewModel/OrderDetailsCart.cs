ff179966f9621a2b8412c066a70736bbc5d3fda9
Ben Spark <bhrugen1990@gmail.com>
2018-02-03 22:19:58 -0600

    Section 10 - OrderDetailsCart View Model

---
+++ TasteRestaurant/ViewModel/OrderDetailsCart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TasteRestaurant.Data;
+
+namespace TasteRestaurant.ViewModel
+{
+    public class OrderDetailsCart
+    {
+        public List<ShoppingCart> listCart { get; set; }
+
+        public  OrderHeader OrderHEader { get; set; }
+
+    }
+}
