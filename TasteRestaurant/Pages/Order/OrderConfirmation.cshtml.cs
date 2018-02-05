64933a23892ef47353649250190cd9d36ccab8a1
Ben Spark <bhrugen1990@gmail.com>
2018-02-05 12:54:42 -0600

    Section 11 - Order Confirmation : Razor Page Model

---
+++ TasteRestaurant/Pages/Order/OrderConfirmation.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TasteRestaurant.Data;
+using TasteRestaurant.ViewModel;
+
+namespace TasteRestaurant.Pages.Order
+{
+    public class OrderConfirmationModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public OrderConfirmationModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public OrderDetailsViewModel OrderDetailsViewModel { get; set; }
+
+        public void OnGet(int id)
+        {
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            OrderDetailsViewModel = new OrderDetailsViewModel(){
+                OrderHeader = _db.OrderHeader.Where(o => o.Id == id && o.UserId==claim.Value).FirstOrDefault(),
+                OrderDetail=_db.OrderDetail.Where(o=>o.OrderId==id).ToList()
+            };
+        }
+    }
+}
