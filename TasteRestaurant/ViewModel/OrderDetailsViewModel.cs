5ac5738bad669c74c08b77ac27f5f52b30ebed03
Ben Spark <bhrugen1990@gmail.com>
2018-02-05 11:42:21 -0600

    Section 11 - Create Order Details ViewModel

---
+++ TasteRestaurant/ViewModel/OrderDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TasteRestaurant.Data;
+
+namespace TasteRestaurant.ViewModel
+{
+    public class OrderDetailsViewModel
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public List<OrderDetail> OrderDetail { get; set; }
+    }
+}
